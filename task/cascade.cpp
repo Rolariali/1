d70033a77ff02db3d931d8a867631c79c9c89b72
Rolariali <Chernomov.E@postuf.com>
2022-02-13 20:35:01 +0300

    cascade test

---
+++ task/cascade.cpp
@@ -0,0 +1,130 @@
+//
+// Created by 1 on 13.02.2022.
+//
+#include "nvcomp/cascaded.hpp"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <vector>
+
+// Test GPU decompression with cascaded compression API //
+
+using namespace std;
+using namespace nvcomp;
+
+#define REQUIRE(a)                                                             \
+  do {                                                                         \
+    if (!(a)) {                                                                \
+      printf("Check " #a " at %d failed.\n", __LINE__);                        \
+      return 0;                                                                \
+    }                                                                          \
+  } while (0)
+
+#define CUDA_CHECK(func)                                                       \
+  do {                                                                         \
+    cudaError_t rt = (func);                                                   \
+    if (rt != cudaSuccess) {                                                   \
+      printf(                                                                  \
+          "API call failure \"" #func "\" with %d at " __FILE__ ":%d\n",       \
+          (int)rt,                                                             \
+          __LINE__);                                                           \
+      return 0;                                                                \
+    }                                                                          \
+  } while (0)
+
+
+// #include "test_data.h"
+
+int main()
+{
+  nvcompCascadedFormatOpts opts;
+//  typedef uint8_t T;
+
+  printf("--------------------------------------------\n");
+  typedef int T;
+  const nvcompType_t type = NVCOMP_TYPE_INT;
+
+  const size_t input_size = 16;
+  T input[16] = {0, 2, 2, 3, 0, 0, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1};
+
+  nvcompCascadedFormatOpts comp_opts;
+  comp_opts.num_RLEs = 1;
+  comp_opts.num_deltas = 0;
+  comp_opts.use_bp = 0;
+  // create GPU only input buffer
+  void* d_in_data;
+  const size_t in_bytes = sizeof(T) * input_size;
+  CUDA_CHECK(cudaMalloc(&d_in_data, in_bytes));
+  CUDA_CHECK(cudaMemcpy(d_in_data, input, in_bytes, cudaMemcpyHostToDevice));
+
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+  nvcompStatus_t status;
+
+  // Compress on the GPU
+  size_t comp_temp_bytes;
+  size_t comp_out_bytes;
+  size_t metadata_bytes;
+  status = nvcompCascadedCompressConfigure(
+      &comp_opts,
+      type,
+      in_bytes,
+      &metadata_bytes,
+      &comp_temp_bytes,
+      &comp_out_bytes);
+  REQUIRE(status == nvcompSuccess);
+
+  void* d_comp_temp;
+  void* d_comp_out;
+  CUDA_CHECK(cudaMalloc(&d_comp_temp, comp_temp_bytes));
+  CUDA_CHECK(cudaMalloc(&d_comp_out, comp_out_bytes));
+
+  size_t* d_comp_out_bytes;
+  CUDA_CHECK(cudaMalloc((void**)&d_comp_out_bytes, sizeof(*d_comp_out_bytes)));
+  CUDA_CHECK(cudaMemcpy(
+      d_comp_out_bytes,
+      &comp_out_bytes,
+      sizeof(*d_comp_out_bytes),
+      cudaMemcpyHostToDevice));
+
+  status = nvcompCascadedCompressAsync(
+      &comp_opts,
+      type,
+      d_in_data,
+      in_bytes,
+      d_comp_temp,
+      comp_temp_bytes,
+      d_comp_out,
+      d_comp_out_bytes,
+      stream);
+  REQUIRE(status == nvcompSuccess);
+  CUDA_CHECK(cudaStreamSynchronize(stream));
+
+  CUDA_CHECK(cudaMemcpy(
+      &comp_out_bytes,
+      d_comp_out_bytes,
+      sizeof(comp_out_bytes),
+      cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaFree(d_comp_out_bytes));
+  CUDA_CHECK(cudaFree(d_comp_temp));
+  CUDA_CHECK(cudaFree(d_in_data));
+
+  // get temp and output size
+  size_t temp_bytes;
+  size_t output_bytes;
+  void* metadata_ptr = NULL;
+
+  vector<uint8_t> out(comp_out_bytes);
+
+  CUDA_CHECK(cudaMemcpy(
+      d_comp_out,
+      out.data(),
+      comp_out_bytes,
+      cudaMemcpyDeviceToHost));
+
+  for(auto el: out)
+    printf("%x:");
+
+  printf("\n\ndone\n");
+}
