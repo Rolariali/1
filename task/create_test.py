808c7ad5fa96bcc6b75ef7a0cd5bdcee8c0c7a7e
Rolariali <Chernomov.E@postuf.com>
2022-02-19 12:28:22 +0300

    bp test

--- task/create_test.py
+++ task/create_test.py
@@ -1,25 +1,40 @@
-import base64
-import json
-import os
 import random
 import sys
 
 
 qty = int(sys.argv[1])
 
-s = ""
+class Data:
 
-for i in range(qty - 1):
-    s += f'{random.randint(1,100)},'
+    s = ""
 
-s += f'{random.randint(1,100)}'
+    def add(self, s: int):
+        self.s += str(s)
 
 
-input = "std::vector<uint8_t> input = { " +  s  + " };"
+def _set_vector(s: str) -> str:
+    input = "std::vector<uint8_t> input = { " +  s  + " };"
+    return input
 
-# print(input)
-with open('task/test_data.h', 'w') as f:
-    f.write(input)
+def write_h(file_name: str, s: str):
+    input = _set_vector(s)
+    with open(f'task/{file_name}.h', 'w') as f:
+        f.write(input)
 
-print('done')
+
+d = Data()
+
+d.add(99)
+
+for _ in range(qty - 1):
+    # s += f'{random.randint(1,100)},'
+    i = random.randint(0,2)
+    d.add(i)
+
+
+write_h("pb99", d.s)
+# s += f'{random.randint(1,100)}'
+
+
+print('done!')
 
