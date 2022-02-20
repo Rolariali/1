40964a0cae50b43c893ff95841537f83665785f3
Rolariali <Chernomov.E@postuf.com>
2022-02-20 20:58:02 +0300

    simple delta test

---
+++ task/delta_test.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 1 on 19.02.2022.
+//
+
+#include "FCascade.h"
+
+bool verbose = true;
+
+int main()
+{
+  vector<uint8_t> input
+      = {96, 97, 97, 97, 97, 97, 97, 97, 98, 98, 98, 98, 98, 98, 98, 98, 98,
+         98, 98, 98, 98, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99};
+  {
+    printf("--------------------Delta------------------------\n");
+
+    struct CompResult res;
+    REQUIRE(FCascade::cascade(input, res, 0, 1, 0));
+    FCascade::show_stat(input, res, true, true);
+
+    printf("--------------------------------------------\n");
+  }
+}
