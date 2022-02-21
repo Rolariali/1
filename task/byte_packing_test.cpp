2803fd64544fb7b95c79e96530cb19f090e703f3
Rolariali <Chernomov.E@postuf.com>
2022-02-21 20:56:13 +0300

    bt test

---
+++ task/byte_packing_test.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 1 on 21.02.2022.
+//
+
+#include "FCascade.h"
+
+bool verbose = true;
+
+int main()
+{
+  vector<uint8_t> input
+      = {96, 97, 97, 97, 97, 97, 97, 97, 98, 98, 98, 98, 98, 98, 98, 98, 98,
+         98, 98, 98, 98, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99};
+  {
+    printf("--------------------Delta------------------------\n");
+
+    struct CompResult res;
+    REQUIRE(FCascade::cascade(input, res, 0, 0, 1));
+    FCascade::show_stat(input, res, true, true);
+
+    printf("--------------------------------------------\n");
+  }
+}
