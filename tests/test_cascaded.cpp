ea9b8f4abea2192f9da79225b272f56342ccc0b3
Rolariali <Chernomov.E@postuf.com>
2022-05-24 02:30:12 +0300

    test loop

--- tests/test_cascaded.cpp
+++ tests/test_cascaded.cpp
@@ -137,12 +137,83 @@ void test_cascaded(const std::vector<T>& input, nvcompType_t data_type)
   cudaFree(out_ptr);
 }
 
+
+template <typename T>
+void test_cascaded_opt(const std::vector<T>& input, const nvcompBatchedCascadedOpts_t options )
+{
+  // create GPU only input buffer
+  T* d_in_data;
+  const size_t in_bytes = sizeof(T) * input.size();
+  CUDA_CHECK(cudaMalloc((void**)&d_in_data, in_bytes));
+  CUDA_CHECK(
+      cudaMemcpy(d_in_data, input.data(), in_bytes, cudaMemcpyHostToDevice));
+
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+//  nvcompBatchedCascadedOpts_t options = nvcompBatchedCascadedDefaultOpts;
+//  options.type = data_type;
+//  options.num_RLEs = 0;
+  CascadedManager manager{options, stream};
+  auto comp_config = manager.configure_compression(in_bytes);
+
+  // Allocate output buffer
+  uint8_t* d_comp_out;
+  CUDA_CHECK(cudaMalloc(&d_comp_out, comp_config.max_compressed_buffer_size));
+
+  manager.compress(
+      reinterpret_cast<const uint8_t*>(d_in_data),
+      d_comp_out,
+      comp_config);
+
+  CUDA_CHECK(cudaStreamSynchronize(stream));
+
+  size_t comp_out_bytes = manager.get_compressed_output_size(d_comp_out);
+
+  cudaFree(d_in_data);
+
+  // Test to make sure copying the compressed file is ok
+  uint8_t* copied = 0;
+  CUDA_CHECK(cudaMalloc(&copied, comp_out_bytes));
+  CUDA_CHECK(
+      cudaMemcpy(copied, d_comp_out, comp_out_bytes, cudaMemcpyDeviceToDevice));
+  cudaFree(d_comp_out);
+  d_comp_out = copied;
+
+  auto decomp_config = manager.configure_decompression(d_comp_out);
+
+  T* out_ptr;
+  cudaMalloc(&out_ptr, decomp_config.decomp_data_size);
+
+  // make sure the data won't match input if not written to, so we can verify
+  // correctness
+  cudaMemset(out_ptr, 0, decomp_config.decomp_data_size);
+
+  manager.decompress(
+      reinterpret_cast<uint8_t*>(out_ptr),
+      d_comp_out,
+      decomp_config);
+  CUDA_CHECK(cudaStreamSynchronize(stream));
+
+  // Copy result back to host
+  std::vector<T> res(input.size());
+  cudaMemcpy(
+      &res[0], out_ptr, input.size() * sizeof(T), cudaMemcpyDeviceToHost);
+
+  // Verify correctness
+  REQUIRE(res == input);
+
+  cudaFree(d_comp_out);
+  cudaFree(out_ptr);
+  cudaStreamDestroy(stream);
+}
+
 } // namespace
 
 /******************************************************************************
  * UNIT TESTS *****************************************************************
  *****************************************************************************/
-
+/*
 TEST_CASE("comp/decomp cascaded-small", "[nvcomp]")
 {
   using T = int;
@@ -261,9 +332,8 @@ std::vector<T> buildRunsPsedoRandom(const size_t numRuns, const size_t runSize)
 #include <thread>
 
 TEST_CASE("comp/decomp memory use stat", "[nvcomp][small]memory")
-{
 //  std::vector<size_t> input_sizes; //= { 1, 33, 1021 };
-
+{
   std::vector<nvcompType_t> data_types = {
       NVCOMP_TYPE_CHAR,
       NVCOMP_TYPE_SHORT,
@@ -292,6 +362,7 @@ TEST_CASE("comp/decomp memory use stat", "[nvcomp][small]memory")
 
 TEST_CASE("simple memory", "simple memory")
 {
+  return;
   using T = int64_t;
   std::vector<T> input(0xFFFF, 3);
   while(1) {
@@ -313,4 +384,35 @@ TEST_CASE("simple memory", "simple memory")
     std::this_thread::sleep_for(1000ms);
     printf("@..");
   }
+}
+*/
+
+static void print_options(const nvcompBatchedCascadedOpts_t & options){
+  printf("chunk_size %zu, rle %d, delta %d, M2Mode %d, bp %d\n",
+         options.chunk_size, options.num_RLEs, options.num_deltas, 0, options.use_bp);
+}
+
+TEST_CASE("comp/decomp cascade find max", "cascade loop of max")
+{
+  std::vector<uint8_t> input = buildRunsPsedoRandom<uint8_t>(1000, 1000);
+  printf("input size: %zu\n", input.size());
+
+  for(size_t chunk_size = 512; chunk_size < 16384; chunk_size += 512)
+    for(int rle = 0; rle < 3; rle++)
+      for(int bp = 0; bp < 2; bp++) {
+        // No delta without BitPack
+        const int max_delta_num = bp == 0 ? 1 : 5;
+        for (int delta = 0; delta < max_delta_num; delta++) {
+          // No delta mode without delta nums
+          const int max_delta_mode = delta == 0 ? 1 : 2;
+          for (int delta_mode = 0; delta_mode < max_delta_mode; delta_mode++) {
+            if ((rle + bp + delta) == 0)
+              continue;
+            const nvcompBatchedCascadedOpts_t options = {chunk_size, NVCOMP_TYPE_UCHAR, rle, delta, bp};
+            print_options(options);
+            test_cascaded_opt(input, options);
+          }
+        }
+      }
+
 }
